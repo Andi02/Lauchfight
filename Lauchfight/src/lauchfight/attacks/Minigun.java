8c44e207a1958173d66ee4e41560882e3243da89
Andi <andreas.daum01@gmail.com>
2018-10-25 14:09:42 +0200

    Minigun finished

--- Lauchfight/src/lauchfight/attacks/Minigun.java
+++ Lauchfight/src/lauchfight/attacks/Minigun.java
@@ -2,6 +2,9 @@ package lauchfight.attacks;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.MouseInfo;
+import java.util.List;
+
 import lauchfight.Attack;
 import lauchfight.LauchFight;
 import lauchfight.Player;
@@ -24,30 +27,40 @@ public class Minigun extends Attack {
 		pSpeed = p.getSpeed();
 		this.p.setSpeed(0f);
 	}
-
 	@Override
-	public Graphics draw(Graphics g) {
-
+	public void phys(List<Attack> newAttack) {
+		Screen.MouseX = (int) MouseInfo.getPointerInfo().getLocation().getX();
+		Screen.MouseY = (int) MouseInfo.getPointerInfo().getLocation().getY();
+		
 		this.x = this.p.getX() + 13;
 		this.y = this.p.getY() + 13;
-
-		// if the attack is used do stuff
-		g.setColor(Color.lightGray);
-		g.drawRect((int) x, (int) y, 25, 25);
-
-		count += 1;
+		
+		count += 2;
 		if (count % 10 == 0) {
-			LauchFight.world.add(Factory.create(this.p));
+			newAttack.add(Factory.create(this.p));
 		}
 		if (count == 1000) {
 			this.setAlive(false);
 			this.p.setSpeed((float) pSpeed);
 		}
+		
+	}
+	@Override
+	public Graphics draw(Graphics g) {
+		
+		
+
+		// if the attack is used do stuff
+		g.setColor(Color.lightGray);
+		g.drawRect((int) x, (int) y, 25, 25);
+
 		return g;
 	}
 
 	@Override
 	public void onCollision(Player playerHit) {
 	}
+
+
 
 }
--- Lauchfight/src/lauchfight/attacks/MinigunProjectile.java
+++ Lauchfight/src/lauchfight/attacks/MinigunProjectile.java
@@ -2,6 +2,7 @@ package lauchfight.attacks;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import lauchfight.Attack;
@@ -12,9 +13,7 @@ import lauchfight.Screen;
 public class MinigunProjectile extends Attack {
 
 	private float speed = 1f;
-	private int rand;
-	private double vX;
-	private double vY;
+	private double angle;
 	int dir = 0;
 
 	public void addX(double a) {
@@ -26,78 +25,46 @@ public class MinigunProjectile extends Attack {
 	}
 
 	public MinigunProjectile(Player pSend) {
-		rand = ThreadLocalRandom.current().nextInt(-100, 100);
+
+		speed += ThreadLocalRandom.current().nextFloat() * 0.02f;
+		width = 6;
+		height = 6;
+
 		double xA = Screen.MouseX - pSend.getX() - 30;
 		double yA = Screen.MouseY - pSend.getY() - 60;
 
-		if (xA <= 0 && Math.abs(xA) > Math.abs(yA)) {
-			if (Math.abs(yA) / Math.abs(xA) > 0.5 && yA >= 0) {
-				dir = 5;
-			} else if (Math.abs(yA) / Math.abs(xA) > 0.5 && yA < 0) {
-				dir = 7;
-			} else {
-				dir = 6;
-			}
-		} else if (xA >= 0 && xA > Math.abs(yA)) {
-			if (Math.abs(yA) / xA > 0.5 && yA >= 0) {
-				dir = 3;
-			} else if (Math.abs(yA) / xA > 0.5 && yA < 0) {
-				dir = 1;
-			} else {
-				dir = 2;
-			}
-		} else if (yA > 0) {
-			if (Math.abs(xA) / Math.abs(yA) > 0.5 && xA >= 0) {
-				dir = 3;
-			} else if (Math.abs(xA) / Math.abs(yA) > 0.5 && xA < 0) {
-				dir = 5;
-			} else {
-				dir = 4;
-			}
-		} else {
-			if (Math.abs(xA) / yA > 0.5 && xA >= 0) {
-				dir = 1;
-			} else if (Math.abs(xA) / yA > 0.5 && xA < 0) {
-				dir = 7;
-			} else {
-				dir = 0;
-			}
-		}
-		if (dir == 0) {xA =  ((double) rand)/1000; yA = -1;}
-		else if (dir == 1);
-		else if (dir == 2){xA = 1; yA = ((double) rand)/1000;}
-		else if (dir == 3);
-			else if (dir == 4){xA =  ((double) rand)/1000; yA = 1;}
-				else if (dir == 5);
-				else if (dir == 6){xA = -1; yA = ((double) rand)/1000;}
-					else if (dir == 7);
-			
+		angle = Math.atan(yA / xA);
+		if (xA < 0)
+			angle += Math.PI;
+		angle += ThreadLocalRandom.current().nextGaussian() * 0.05;
+
 		// the aim position
 
 		this.x = pSend.getX() + 25 - width / 2;
 		this.y = pSend.getY() + 25 - height / 2;
-
-		double k = Math.sqrt((speed * speed) / (xA * xA + yA * yA));
-		vX = xA * k;
-		vY = yA * k;
 
 		// save the player that created the attack
 		this.p = pSend;
 	}
 
 	@Override
-	public Graphics draw(Graphics g) {
-
-		// if the attack is used do stuff
-		addX(vX);
-		addY(vY);
-		
-		g.setColor(Color.lightGray);
-		g.fillRect((int) x, (int) y, width, height);
+	public void phys(List<Attack> newAttack) {
+		addX(speed * Math.cos(angle));
+		addY(speed * Math.sin(angle));
 
 		if (this.x >= LauchFight.screenX || this.y >= LauchFight.screenY || this.x <= 0 || this.y <= 0) {
 			this.setAlive(false);
 		}
+	}
+
+	@Override
+	public Graphics draw(Graphics g) {
+
+		// if the attack is used do stuff
+
+		g.setColor(Color.lightGray);
+		g.fillRect((int) x - 5, (int) y - 5, width, height);
+		g.fillRect((int) x, (int) y, width, height);
 
 		return g;
 	}
@@ -107,7 +74,7 @@ public class MinigunProjectile extends Attack {
 		// do stuff with the player if it gets hit!
 
 		if (playerHit != this.p) {
-			playerHit.addLife(-1);
+			playerHit.addLife(-2);
 			this.setAlive(false);
 		}
 
