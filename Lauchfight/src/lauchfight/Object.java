0e8700428ad125ac8137cabec47c59bcc62e70cd
NBI-WebC <christoph02.cw@gmail.com>
2018-10-29 12:09:57 +0100

    .

--- Lauchfight/src/lauchfight/Object.java
+++ Lauchfight/src/lauchfight/Object.java
@@ -43,7 +43,7 @@ public abstract class Object {
         yPos = 0;
         this.r = 0;
         this.g = 0;
-        this.b = 255;
+        this.b = 0;
         shape = "rect";
         alive = true;
     }
